7023825e637309709125cb154a5b0c20633f537d
Tenis de Mesa Içara <contato@tenisdemesa.biz>
2025-09-20 08:16:09 -0300

    Add server/static.ts - separate vite from production code

---
+++ server/static.ts
@@ -0,0 +1,30 @@
+import express, { type Express } from "express";
+import fs from "fs";
+import path from "path";
+
+export function log(message: string, source = "express") {
+  const formattedTime = new Date().toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
+
+  console.log(`${formattedTime} [${source}] ${message}`);
+}
+
+export function serveStatic(app: Express) {
+  const distPath = path.resolve("dist/public");
+
+  if (!fs.existsSync(distPath)) {
+    throw new Error(
+      `Could not find the frontend build! Did you forget to run the build command?`
+    );
+  }
+
+  app.use(express.static(distPath));
+
+  app.get("*", (_req, res) => {
+    res.sendFile(path.resolve(distPath, "index.html"));
+  });
+}
